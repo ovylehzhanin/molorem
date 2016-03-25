93d263adf79f0362b45e4e4b23cc80271fe6bf03
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2016-03-25 15:19:17 +0200

    Gradient background; Delete excess code

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 window.onload = function() {
   var LOREM_DATA = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
   var requestSize = document.getElementById('input');
-  // var goAction = document.getElementById('go'); <-- delete this shit?
-
   /* Describe function that blocks invalid input data */
   document.getElementsByTagName('input')[0].onkeypress = function(e) {
 
@@ -58,18 +56,4 @@ window.onload = function() {
     resultOutput.innerHTML = result;
   }
 
-  /* OLD */
-  // goAction.onclick = function() {
-  //   var inputData = document.getElementById('input');
-  //   var resultData = document.getElementById('result');
-  //   var result = "";
-  //   resultData.innerHTML = "";
-  //
-  //   for (var i = 0; i < +inputData.value ; i++) {
-  //     result += LOREM_DATA[i];
-  //   }
-  //
-  //   resultData.innerHTML = result;
-  // }
-  /* <--end--> */
 }
